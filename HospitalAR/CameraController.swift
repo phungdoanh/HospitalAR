c96c17586475bf0bcbaa72be26af1cf020e1f6ab
Peter Andringa <peter@andrin.ga>
2018-02-12 15:04:26 -0500

    Working on-device demo

--- HospitalAR/CameraController.swift
+++ HospitalAR/CameraController.swift
@@ -8,18 +8,30 @@
 import UIKit
 import AVFoundation
 
-class CameraController: UIViewController {
+class CameraController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+    @IBOutlet var popupView:UIView!
     @IBOutlet var messageLabel:UILabel!
+    @IBOutlet weak var messageConstraint: NSLayoutConstraint!
     
     var captureSession = AVCaptureSession()
     
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
     var qrCodeFrameView: UIView?
+    
+    var popupVisible = false;
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Set up popup and move offscreen
+        // Set up popup and move offscreen
+        popupView.layer.shadowOpacity = 0.5
+        popupView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        popupView.layer.shadowRadius = 3
+        popupView.layer.shadowColor = UIColor.lightGray.cgColor
+        messageConstraint.constant = -self.view.frame.size.height
+        
         // Get the back-facing camera for capturing videos
         let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
         
@@ -58,9 +70,6 @@ class CameraController: UIViewController {
         // Start video capture.
         captureSession.startRunning()
         
-        // Move the message label and top bar to the front
-        view.bringSubview(toFront: messageLabel)
-        
         // Initialize QR Code Frame to highlight the QR code
         qrCodeFrameView = UIView()
         
@@ -70,16 +79,13 @@ class CameraController: UIViewController {
             view.addSubview(qrCodeFrameView)
             view.bringSubview(toFront: qrCodeFrameView)
         }
+        
+        // Move the message label and top bar to the front
+        view.bringSubview(toFront: messageLabel)
+        view.bringSubview(toFront: popupView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-}
-
-extension CameraController: AVCaptureMetadataOutputObjectsDelegate {
-    
+    // Called when QR Code is Detected
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         // Check if the metadataObjects array is not nil and it contains at least one object.
         if metadataObjects.count == 0 {
@@ -93,11 +99,45 @@ extension CameraController: AVCaptureMetadataOutputObjectsDelegate {
         
         let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
         qrCodeFrameView?.frame = barCodeObject!.bounds
-            
+        
         if let codeUrl = metadataObj.stringValue {
             let c = Clue(url: codeUrl)
             messageLabel.text = c.id
+            if(!popupVisible){
+                showPopup()
+            }
+            
         }
     }
     
+    func showPopup() {
+        popupVisible = true;
+        print("Popping up!")
+        messageConstraint.constant = 0;
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func hidePopup(){
+        popupVisible = false;
+        messageConstraint.constant = -self.view.frame.size.height;
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    @IBAction func statusTap(_ sender: Any) {
+        showPopup()
+    }
+    
+    @IBAction func nextClueTap(_ sender: Any) {
+        hidePopup()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
