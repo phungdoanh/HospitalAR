083a4b7b01e408d9a86fa7d50f1d8a1160b7e7b2
Andrea Gonzales <andreafgonzales@gmail.com>
2018-03-04 14:54:46 -0500

    initial character customize ui

---
+++ HospitalAR/Hero.swift
@@ -0,0 +1,9 @@
+//
+//  Hero.swift
+//  HospitalAR
+//
+//  Created by Andrea Gonzales on 2/25/18.
+//  Copyright © 2018 Andrea Gonzales. All rights reserved.
+//
+
+import Foundation
---
+++ HospitalAR/NameController.swift
@@ -0,0 +1,9 @@
+//
+//  NameController.swift
+//  HospitalAR
+//
+//  Created by Andrea Gonzales on 2/26/18.
+//  Copyright © 2018 Andrea Gonzales. All rights reserved.
+//
+
+import Foundation
