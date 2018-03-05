06566dfb6d48c84040b56f968c461d8bd0cfe69c
Andrea Gonzales <andreafgonzales@gmail.com>
2018-03-04 20:37:27 -0500

    WIP programmatically adding buttons, need to merge first

--- HospitalAR/Hero.swift
+++ HospitalAR/Hero.swift
@@ -13,9 +13,9 @@ class Hero : Object {
     @objc dynamic var id = 0
     @objc dynamic var name = ""
     var completedClues = List<Int>()
-    var hair = Hair.blond
-    var skin = Skin.white
-    var suit = Suit.red
+    var hair = Hair.Blond
+    var skin = Skin.White
+    var suit = Suit.Red
 //    var hair: Hair {
 //        get {
 //            if let a = Hair(rawValue: 1) {
@@ -29,27 +29,51 @@ class Hero : Object {
     override static func primaryKey() -> String? {
         return "id"
     }
-}
-
-enum Hair : String {
-    case blond = "blond"
-    case red = "red"
-    case brown = "brown"
-    case black = "black"
-}
-
-enum Skin : String {
-    case white = "white"
-    case light = "light"
-    case tan = "tan"
-    case dark = "dark"
-}
-
-enum Suit : String {
-    case red = "red"
-    case green = "green"
-    case blue = "blue"
-    case purple = "purple"
+    
+    enum Hair {
+        case Blond, Red, Brown, Black
+        
+        var uiColor: UIColor {
+            switch self {
+                case .Blond: return UIColor.init(red: 255, green: 210, blue: 105, alpha: 1)
+                case .Red: return UIColor.init(red: 207, green: 79, blue: 44, alpha: 1)
+                case .Brown: return UIColor.init(red: 133, green: 72, blue: 50, alpha: 1)
+                case .Black: return UIColor.black
+            }
+        }
+        
+        static let array = [Blond, Red, Brown, Black]
+    }
+    
+    enum Skin {
+        case White, Light, Tan, Dark
+        
+        var uiColor: UIColor {
+            switch self {
+                case .White: return UIColor.init(red: 255, green: 242, blue: 228, alpha: 1)
+                case .Light: return UIColor.init(red: 255, green: 227, blue: 193, alpha: 1)
+                case .Tan: return UIColor.init(red: 205, green: 152, blue: 122, alpha: 1)
+                case .Dark: return UIColor.init(red: 141, green: 104, blue: 84, alpha: 1)
+            }
+        }
+        
+        static let array = [White, Light, Tan, Dark]
+    }
+    
+    enum Suit {
+        case Red, Green, Blue, Purple
+        
+        var uiColor: UIColor {
+            switch self {
+            case .Red: return UIColor.init(red: 210, green: 36, blue: 20, alpha: 1)
+            case .Green: return UIColor.init(red: 70, green: 191, blue: 64, alpha: 1)
+            case .Blue: return UIColor.init(red: 50, green: 55, blue: 191, alpha: 1)
+            case .Purple: return UIColor.init(red: 167, green: 33, blue: 191, alpha: 1)
+            }
+        }
+        
+        static let array = [red, green, blue, purple]
+    }
 }
 
 
--- HospitalAR/HeroCustomizeController.swift
+++ HospitalAR/HeroCustomizeController.swift
@@ -17,6 +17,15 @@ class HeroCustomizeController : UIViewController {
     @IBOutlet weak var hairLabel: UILabel!
     @IBOutlet weak var suitLabel: UILabel!
     
+    @IBOutlet weak var suitView: UIView!
+    @IBOutlet weak var skinView: UIView!
+    @IBOutlet weak var hairView: UIView!
+    
+    @IBOutlet weak var skinToggle: UIButton!
+    @IBOutlet weak var hairToggle: UIButton!
+    @IBOutlet weak var suitToggle: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,65 +47,97 @@ class HeroCustomizeController : UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func skinWhiteClick(_ sender: UIButton) {
-        hero.skin = Skin.white
-        skinLabel.text = "SKIN: " + hero.skin.rawValue
+    func changeToHairBtn(btn: UIButton, hair: Hero.Hair) {
+        btn.backgroundColor = hair.uiColor
+        btn.addAc
     }
     
-    @IBAction func skinLightClick(_ sender: UIButton) {
-        hero.skin = Skin.light
-        skinLabel.text = "SKIN: " + hero.skin.rawValue
+    func changeToSkinBtn(btn: UIButton, skin: Hero.Skin) {
+        btn.backgroundColor = skin.uiColor
     }
     
-    @IBAction func skinTanClick(_ sender: UIButton) {
-        hero.skin = Skin.tan
-        skinLabel.text = "SKIN: " + hero.skin.rawValue
+    func changeToSuitBtn(btn: UIButton, suit: Hero.Suit) {
+        btn.backgroundColor = suit.uiColor
+    }
+//
+//    @IBAction func skinWhiteClick(_ sender: UIButton) {
+//        hero.skin = Trait.Skin.white
+//        skinLabel.text = "SKIN: " + hero.skin.rawValue
+//    }
+//
+//    @IBAction func skinLightClick(_ sender: UIButton) {
+//        hero.skin = Trait.Skin.light
+//        skinLabel.text = "SKIN: " + hero.skin.rawValue
+//    }
+//
+//    @IBAction func skinTanClick(_ sender: UIButton) {
+//        hero.skin = Trait.Skin.tan
+//        skinLabel.text = "SKIN: " + hero.skin.rawValue
+//    }
+//
+//    @IBAction func skinDarkClick(_ sender: UIButton) {
+//        hero.skin = Trait.Skin.dark
+//        skinLabel.text = "SKIN: " + hero.skin.rawValue
+//    }
+//
+//    //HAIR
+//    @IBAction func hairBlondClick(_ sender: UIButton) {
+//        hero.hair = Trait.Hair.blond
+//        hairLabel.text = "HAIR: " + hero.hair.rawValue
+//    }
+//
+//    @IBAction func hairRedClick(_ sender: UIButton) {
+//        hero.hair = Trait.Hair.red
+//        hairLabel.text = "HAIR: " + hero.hair.rawValue
+//    }
+//
+//    @IBAction func hairBrownClick(_ sender: UIButton) {
+//        hero.hair = Trait.Hair.brown
+//        hairLabel.text = "HAIR: " + hero.hair.rawValue
+//    }
+//
+//    @IBAction func hairBlackClick(_ sender: UIButton) {
+//        hero.hair = Trait.Hair.black
+//        hairLabel.text = "HAIR: " + hero.hair.rawValue
+//    }
+//
+//    //SUIT
+//    @IBAction func suitRedClick(_ sender: UIButton) {
+//        hero.suit = Suit.red
+//        suitLabel.text = "SUIT: " + hero.suit.rawValue
+//    }
+//
+//    @IBAction func suitGreenClick(_ sender: UIButton) {
+//        hero.suit = Suit.green
+//        suitLabel.text = "SUIT: " + hero.suit.rawValue
+//    }
+//
+//    @IBAction func suitBlueClick(_ sender: UIButton) {
+//        hero.suit = Suit.blue
+//        suitLabel.text = "SUIT: " + hero.suit.rawValue
+//    }
+//
+//    @IBAction func suitPurpleClick(_ sender: UIButton) {
+//        hero.suit = Suit.purple
+//        suitLabel.text = "SUIT: " + hero.suit.rawValue
+//    }
+    
+    //SHOW & HIDE VIEWS
+    @IBAction func toggleSkin(_ sender: UIButton) {
+        suitView.isHidden = !suitView.isHidden;
+        hairToggle.isHidden = !hairToggle.isHidden;
+        hairView.isHidden = !hairView.isHidden;
+        suitToggle.isHidden = !suitToggle.isHidden;
+        suitView.isHidden = !suitView.isHidden;
     }
     
-    @IBAction func skinDarkClick(_ sender: UIButton) {
-        hero.skin = Skin.dark
-        skinLabel.text = "SKIN: " + hero.skin.rawValue
+    @IBAction func toggleHair(_ sender: UIButton) {
+        hairView.isHidden = !suitView.isHidden;
+        hairToggle.isHidden = !hairToggle.isHidden;
+        suitToggle.isHidden = !suitToggle.isHidden;
     }
     
-    //HAIR
-    @IBAction func hairBlondClick(_ sender: UIButton) {
-        hero.hair = Hair.blond
-        hairLabel.text = "HAIR: " + hero.hair.rawValue
-    }
-    
-    @IBAction func hairRedClick(_ sender: UIButton) {
-        hero.hair = Hair.red
-        hairLabel.text = "HAIR: " + hero.hair.rawValue
-    }
-    
-    @IBAction func hairBrownClick(_ sender: UIButton) {
-        hero.hair = Hair.brown
-        hairLabel.text = "HAIR: " + hero.hair.rawValue
-    }
-    
-    @IBAction func hairBlackClick(_ sender: UIButton) {
-        hero.hair = Hair.black
-        hairLabel.text = "HAIR: " + hero.hair.rawValue
-    }
-    
-    //SUIT
-    @IBAction func suitRedClick(_ sender: UIButton) {
-        hero.suit = Suit.red
-        suitLabel.text = "SUIT: " + hero.suit.rawValue
-    }
-    
-    @IBAction func suitGreenClick(_ sender: UIButton) {
-        hero.suit = Suit.green
-        suitLabel.text = "SUIT: " + hero.suit.rawValue
-    }
-    
-    @IBAction func suitBlueClick(_ sender: UIButton) {
-        hero.suit = Suit.blue
-        suitLabel.text = "SUIT: " + hero.suit.rawValue
-    }
-    
-    @IBAction func suitPurpleClick(_ sender: UIButton) {
-        hero.suit = Suit.purple
-        suitLabel.text = "SUIT: " + hero.suit.rawValue
+    @IBAction func toggleSuit(_ sender: UIButton) {
+        
     }
 }
